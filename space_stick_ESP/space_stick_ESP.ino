bc1d9e49ccd65db9eee8a3b73df77f2f9dd984a8
VinterMute <vinter101010@gmail.com>
2020-10-18 21:23:51 +0700

    added sync

--- space_stick_ESP/space_stick_ESP.ino
+++ space_stick_ESP/space_stick_ESP.ino
@@ -3,6 +3,8 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <FastLED.h>
+#include <EEPROM.h>
+
 #include "colorutils.h"
 #include "colorpalettes.h"
 #include "GyverButton.h"
@@ -57,9 +59,9 @@ uint8_t ran1;
 uint8_t ran2;
 
 //three_sin_demo parameters
-int wave1=0;                // Current phase is calculated.
-int wave2=0;
-int wave3=0;
+int wave1 = 0;              // Current phase is calculated.
+int wave2 = 0;
+int wave3 = 0;
 
 uint8_t inc1 = 2;           // Phase shift. Keep it low, as this is the speed at which the waves move.
 uint8_t inc2 = 1;
@@ -72,7 +74,7 @@ uint8_t lvl3 = 80;
 uint8_t mul1 = 20;          // Frequency, thus the distance between waves
 uint8_t mul2 = 25;
 uint8_t mul3 = 22;
-uint8_t test = 23;
+
 
 //...parameters
 const char *ssid = APSSID;
@@ -82,14 +84,77 @@ ESP8266WebServer server(80);
 
 void setup() {
   delay(1000);
-
   Serial.begin(115200);
-  Serial.print("Configuring access point...");
-  WiFi.softAP(ssid, password);
 
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+  EEPROM.begin(512);
+  byte flag_mode_wifi = EEPROM.read(0);
+  Serial.print("FLAG_WIFI_");
+  Serial.println(flag_mode_wifi);
+
+
+  //Если флаг равен 1 то режим клиента wifi
+
+
+  if (flag_mode_wifi == 1) {
+    Serial.println("FLAG IS 1");
+    //read ssid and passwd
+    byte ssid_size = EEPROM.read(1);
+    byte password_size = EEPROM.read(2);
+    char ssid_wifi[ssid_size + 1];
+    char password_wifi[password_size + 1];
+
+    Serial.print("SSID_IS_");
+    //Считываем ssid из памяти
+    for (int i = 3; i < ssid_size + 3; ++i ) {
+      ssid_wifi[i - 3] = (char) EEPROM.read(i) + '0';
+      Serial.print((char) EEPROM.read(i));
+    }
+    Serial.println();
+    Serial.print("PASSWD_IS_");
+    //Считываем пароль из памяти
+    for (int i = 3 + ssid_size; i < ssid_size + 3 + password_size; ++i ) {
+      password_wifi[i - 3 - ssid_size] = (char) EEPROM.read(i) + '0';
+      Serial.print((char) EEPROM.read(i));
+    }
+    Serial.println();
+
+    //
+
+    //connect
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(String(ssid_wifi), String(password_wifi));//вот эти переменные нужно тащить из памяти
+    Serial.println(String(ssid_wifi));
+    Serial.println(String(password_wifi));
+
+    EEPROM.write(0, 0);//Меняем флаг  для смены режима в точку доступа после перезагрузки
+    EEPROM.commit();
+
+    while (WiFi.status() != WL_CONNECTED) { // ЗДЕСЬ ПРОГРАММА ВСТАНЕТ
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    
+    
+  }
+  else {
+    Serial.print("Configuring access point...");
+    WiFi.softAP(ssid, password);
+
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+
+  }
+
+
+  
+
 
   //API methods
   server.on("/api/check", HTTP_GET, handleCheckConnection);
@@ -97,7 +162,10 @@ void setup() {
   server.on("/api/mode/speed", HTTP_POST, handleSpeed);
   server.on("/api/mode/brightness", HTTP_POST, handleBrightness);
   server.on("/api/mode/pattern/color", HTTP_POST, handleColor);
-  
+
+  server.on("/api/switch/mode", HTTP_POST, handleSwitch);
+
+
   server.begin();
 
   Serial.println("HTTP server started");
@@ -111,21 +179,21 @@ void setup() {
 void loop() {
 
   server.handleClient();
-//
-//  butt1.tick();  // обязательная функция отработки. Должна постоянно опрашиваться
-//  if (butt1.isClick()) {           // одиночное нажатие  и lenMode не больше количества режимов
-//    ledMode=(ledMode+1) % NUM_MODES;              // инкремент
-//   // Serial.print(ledMode);
-//  }
-//
-//  if ( BRIGHTNESS >= MAX_BRIGHTNESS){
-//    BRIGHTNESS=10;
-//  }
-//
-//  if (butt1.isStep()) {            // обработчик удержания с шагами
-//    SPEED+=10;                       // увеличивать/уменьшать переменную value с шагом и интервалом!
-//   // Serial.print(SPEED);
-//  }
+  //
+  //  butt1.tick();  // обязательная функция отработки. Должна постоянно опрашиваться
+  //  if (butt1.isClick()) {           // одиночное нажатие  и lenMode не больше количества режимов
+  //    ledMode=(ledMode+1) % NUM_MODES;              // инкремент
+  //   // Serial.print(ledMode);
+  //  }
+  //
+  //  if ( BRIGHTNESS >= MAX_BRIGHTNESS){
+  //    BRIGHTNESS=10;
+  //  }
+  //
+  //  if (butt1.isStep()) {            // обработчик удержания с шагами
+  //    SPEED+=10;                       // увеличивать/уменьшать переменную value с шагом и интервалом!
+  //   // Serial.print(SPEED);
+  //  }
 
   switch (ledMode) {
     case 0: rainbow(); break;
@@ -152,7 +220,7 @@ void loop() {
 
     case 11: three_sin(); break;
 
-    case 12: thisdelay = 25; mul1 = 20; mul2 = 25; mul3 = 22; lvl1 = 80; lvl2 = 80; lvl3 = 80; inc1 = 1; inc2 = 1;three_sin(); break;
+    case 12: thisdelay = 25; mul1 = 20; mul2 = 25; mul3 = 22; lvl1 = 80; lvl2 = 80; lvl3 = 80; inc1 = 1; inc2 = 1; three_sin(); break;
 
     case 13: mul1 = 5; mul2 = 8; mul3 = 7; three_sin(); break;
 
@@ -176,20 +244,20 @@ void loop() {
 
 //API handlers
 void handleCheckConnection() {
-	server.send(200);
+  server.send(200);
 }
 
 void handlePattern() {
-	if (server.hasArg("pattern")) {
-		ledMode=(atoi(server.arg("pattern").c_str()));
+  if (server.hasArg("pattern")) {
+    ledMode = (atoi(server.arg("pattern").c_str()));
     server.send(200);
     Serial.println(ledMode);
-	}
+  }
 }
 
 void handleSpeed() {
   if (server.hasArg("s")) {
-    SPEED=(atoi(server.arg("s").c_str()));
+    SPEED = (atoi(server.arg("s").c_str()));
     server.send(200);
     Serial.println(SPEED);
   }
@@ -197,7 +265,7 @@ void handleSpeed() {
 
 void handleBrightness() {
   if (server.hasArg("b")) {
-    BRIGHTNESS=(atoi(server.arg("b").c_str()));
+    BRIGHTNESS = (atoi(server.arg("b").c_str()));
     server.send(200);
     Serial.println(BRIGHTNESS);
   }
@@ -205,16 +273,56 @@ void handleBrightness() {
 
 void handleColor() {
   if (server.hasArg("r") && server.hasArg("g") && server.hasArg("b")) {
-  	fill_solid(leds, NUM_LEDS, CRGB(
-      atoi(server.arg("b").c_str()),
-  		atoi(server.arg("g").c_str()),
-  		atoi(server.arg("r").c_str())));
-    
+    fill_solid(leds, NUM_LEDS, CRGB(
+                 atoi(server.arg("b").c_str()),
+                 atoi(server.arg("g").c_str()),
+                 atoi(server.arg("r").c_str())));
+
     server.send(200);
   }
 }
 
-void constColor(){
+void handleSwitch() {
+  if (server.hasArg("ssid") && server.hasArg("password")) {
+    byte ssid_size = strlen(server.arg("ssid").c_str());
+    byte password_size = strlen(server.arg("password").c_str());
+
+    const char* ssid_wifi = new char[ssid_size];
+    const char* password_wifi = new char[password_size];
+    
+     ssid_wifi = server.arg("ssid").c_str();
+     password_wifi = server.arg("password").c_str();
+     
+
+    //Поднимаем флаг и пишем размеры ssid и пароля в первые 3 ячейки
+    Serial.println("Поднимаем флаг");
+    EEPROM.write(0, 1);
+    EEPROM.write(1, ssid_size);
+    EEPROM.write(2, password_size);
+    //Записываем ssid в  eeprom
+    for (int i = 3; i < ssid_size + 3 ; i++) {
+      EEPROM.write(i, ssid_wifi[i - 3]);
+      Serial.print("ЗАпиСали ____");
+      Serial.print(ssid_wifi[i - 3]);
+    }
+    
+    Serial.println();// Разделитель вывода
+    
+    //Записываем пароль в  eeprom
+    for (int i = ssid_size + 3; i < ssid_size + password_size + 3; i++) {
+      EEPROM.write(i, password_wifi[i - 3 - ssid_size]);
+      Serial.print("ЗАпиСали ____");
+      Serial.print(password_wifi[i - 3 - ssid_size]);
+    }
+  EEPROM.commit();
+  server.send(200);
+  delay(500);
+  ESP.restart();
+ 
+  }
+}
+
+void constColor() {
 }
 
 //Space stick patterns
@@ -230,31 +338,31 @@ void three_sin() {
   wave1 += inc1;
   wave2 += inc2;
   wave3 += inc3;
-  for (int k=0; k<NUM_LEDS; k++) {
-    leds[k].r = qsub8(sin8(mul1*k + wave1/128), lvl1);        // Another fixed frequency, variable phase sine wave with lowered level
-    leds[k].g = qsub8(sin8(mul2*k + wave2/128), lvl2);        // A fixed frequency, variable phase sine wave with lowered level
-    leds[k].b = qsub8(sin8(mul3*k + wave3/128), lvl3);        // A fixed frequency, variable phase sine wave with lowered level
+  for (int k = 0; k < NUM_LEDS; k++) {
+    leds[k].r = qsub8(sin8(mul1 * k + wave1 / 128), lvl1);    // Another fixed frequency, variable phase sine wave with lowered level
+    leds[k].g = qsub8(sin8(mul2 * k + wave2 / 128), lvl2);    // A fixed frequency, variable phase sine wave with lowered level
+    leds[k].b = qsub8(sin8(mul3 * k + wave3 / 128), lvl3);    // A fixed frequency, variable phase sine wave with lowered level
   }
 }
 
 void rainbow() {
   // FastLED's built-in rainbow generator
-  if (thisdir == 0) thishue+= thisrot; else thishue -= thisrot; // I could use signed math, but 'thisdir' works with other routines.
+  if (thisdir == 0) thishue += thisrot; else thishue -= thisrot; // I could use signed math, but 'thisdir' works with other routines.
   fill_rainbow(leds, NUM_LEDS, thishue, deltahue);
-  // fadeToBlackBy( leds, NUM_LEDS, 255-BRIGHTNESS); 
+  // fadeToBlackBy( leds, NUM_LEDS, 255-BRIGHTNESS);
 }
 
 void blendwave() {
 
-  speed = beatsin8(6,0,200);
+  speed = beatsin8(6, 0, 200);
 
-  clr1 = blend(CHSV(beatsin8(3,0,200),200,200), CHSV(beatsin8(4,0,200),200,200), speed);
-  clr2 = blend(CHSV(beatsin8(4,0,200),200,200), CHSV(beatsin8(3,0,200),200,200), speed);
+  clr1 = blend(CHSV(beatsin8(3, 0, 200), 200, 200), CHSV(beatsin8(4, 0, 200), 200, 200), speed);
+  clr2 = blend(CHSV(beatsin8(4, 0, 200), 200, 200), CHSV(beatsin8(3, 0, 200), 200, 200), speed);
 
-  loc1 = beatsin8(10,0,NUM_LEDS-1);
-  
+  loc1 = beatsin8(10, 0, NUM_LEDS - 1);
+
   fill_gradient_RGB(leds, 0, clr2, loc1, clr1);
-  fill_gradient_RGB(leds, loc1, clr2, NUM_LEDS-1, clr1);
+  fill_gradient_RGB(leds, loc1, clr2, NUM_LEDS - 1, clr1);
 
 }
 
